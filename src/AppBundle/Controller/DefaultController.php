2feb72d6d8eace0494da8bc70e8a80fe6ca15fe3
Gabriel <Gabriel@Unasus-PC33>
2017-05-26 11:56:30 -0300

    Menu lateral e página inicial

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -29,4 +29,16 @@ class DefaultController extends Controller
         ]
         );
     }
+
+    /**
+     * @Route("/home")
+     */
+    public function home(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]
+        );
+    }
 }
