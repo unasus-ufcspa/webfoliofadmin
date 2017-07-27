d87b2e42cbbe4d221db29764082be3eaa0a922be
Marilia Rosa <mariliarosilva@gmail.com>
2017-07-27 11:46:29 -0300

    arquivo js cadastro turma

---
+++ web/assets/js/cadastroTurma.js
@@ -0,0 +1,20 @@
+$("#formCadastroTurma").submit(function(e) {
+    var dataString = {
+        arrayAdministradoresDesativar: usuariosExcecao.usuariosExcecao
+    };
+    console.log(dataString);
+    $.ajax({
+        type: 'post',
+        data: JSON.stringify(dataString),
+        contentType: 'application/json',
+        dataType: 'json',
+        url: '' + caminho + 'efetuarCadastroTurma',
+        cache: false,
+        processData: false,
+        async: false,
+        success: function (response) {
+            console.log(response);
+            location.reload();
+        }
+    });
+});
