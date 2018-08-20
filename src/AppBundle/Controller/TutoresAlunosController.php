d965a2b001b7ff5e4250165c20fbe1a3491a2e57
Gabriel <gabrielfleal98@gmail.com>
2018-08-20 10:53:27 -0300

    Tutor Aluno

--- src/AppBundle/Controller/TutoresAlunosController.php
+++ src/AppBundle/Controller/TutoresAlunosController.php
@@ -16,6 +16,9 @@ use AppBundle\Controller\UsuarioController;
 use AppBundle\Controller\ManipularArquivoController;
 use AppBundle\Entity\TbUser;
 use AppBundle\Entity\TbClassStudent;
+use AppBundle\Entity\TbPortfolioStudent;
+use AppBundle\Entity\TbTutorPortfolio;
+use AppBundle\Entity\TbPortfolioClass;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
@@ -69,9 +72,55 @@ class TutoresAlunosController extends Controller {
 
     }
 
-    function registrarAlunosTutor(){
+    function registrarAlunosTutor($dadosForm){
       $idClass = $this->get('session')->get('idTurmaEdicao');
+      $arrays = explode(";", $dadosForm['IdAlunosTutores']);
 
-      // $newTutorPortfolio = Tb
+      foreach ($arrays as $a) {
+        $idUsers = explode(".", $a);
+
+        $tutor = $this->getDoctrine()
+                ->getRepository('AppBundle:TbUser')
+                ->findOneBy(array('idUser' => $idUsers[0]));
+
+        for($i = 1; $i < sizeof($idUsers); $i++){
+
+          $queryBuilderPortStudent = $this->em->createQueryBuilder();
+          $queryBuilderPortStudent
+                  ->select('ps')
+                  ->from('AppBundle:TbPortfolioStudent', "ps")
+                  ->innerJoin('ps.idPortfolioClass', 'pc', 'WITH', 'ps.idPortfolioClass = pc.idPortfolioClass')
+                  ->where($queryBuilderPortStudent->expr()->eq('pc.idClass', $idClass))
+                  ->andWhere($queryBuilderPortStudent->expr()->eq('ps.idStudent', $idUsers[$i]))
+                  ->getQuery()
+                  ->execute();
+          $portfolioStudent = $queryBuilderPortStudent->getQuery()->getArrayResult();
+
+          if ($portfolioStudent != null) {
+            $this->logControle->logAdmin("Point 2");
+            $queryBuilderTutorPort = $this->em->createQueryBuilder();
+            $queryBuilderTutorPort
+                    ->select('tp')
+                    ->from('AppBundle:TbTutorPortfolio', "tp")
+                    ->where($queryBuilderTutorPort->expr()->eq('tp.idPortfolioStudent', $portfolioStudent[0]['idPortfolioStudent']))
+                    ->getQuery()
+                    ->execute();
+            $tutorPort = $queryBuilderTutorPort->getQuery()->getResult();
+
+            if($tutorPort == null){
+              $ps = $this->getDoctrine()
+                      ->getRepository('AppBundle:TbPortfolioStudent')
+                      ->findOneBy(array('idPortfolioStudent' => $portfolioStudent[$i]));
+
+              $novoTutorPort = new TbTutorPortfolio();
+              $novoTutorPort->setIdTutor($tutor);
+              $novoTutorPort->setIdPortfolioStudent($ps);
+              $this->em->persist($novoTutorPort);
+              $this->em->flush();
+            }
+          }
+        }
+      }
+      $this->em->flush();
     }
 }
